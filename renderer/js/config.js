c1cee0470b6022263943ad557559e7fdc821e265
Napong Dungduangsasitorn <jamesnapongd@gmail.com>
2020-07-31 21:08:48 +0700

    private

---
+++ renderer/js/config.js
@@ -0,0 +1,16 @@
+/**
+ *   @author Napong Dungduangsasitorn
+ * */
+// Your web app's Firebase configuration
+var sercetKey = {
+  apiKey: "AIzaSyAvX0t8hK6F9ihmlg02aa_93UaVijrG3zE",
+  authDomain: "able-c8fd8.firebaseapp.com",
+  databaseURL: "https://able-c8fd8.firebaseio.com",
+  projectId: "able-c8fd8",
+  storageBucket: "gs://able-c8fd8.appspot.com/",
+  messagingSenderId: "798202959863",
+  appId: "1:798202959863:web:a189bcd23813be66"
+};
+
+// Fix email to login
+const allowedEmailDomain = 'ableprimpton.com';
